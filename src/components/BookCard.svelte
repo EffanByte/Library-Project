7815ef7c857aa12447f10aa23cbfce6b155f50aa
EffanByte <94035974+EffanByte@users.noreply.github.com>
2023-11-18 02:50:51 +0500

    Added relevant components

---
+++ src/components/BookCard.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let book; // Receiving book data as a prop
+</script>
+
+<div class="card" style="width: 18rem;">
+    <img src={book.coverUrl} class="card-img-top" alt={`Cover of ${book.title}`} />
+    <div class="card-body">
+        <h5 class="card-title">{book.title}</h5>
+    </div>
+</div>
---
+++ src/components/navbar.svelte
@@ -0,0 +1,26 @@
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <!-- Logo -->
+        <a class="navbar-brand" href="#">
+            Library Management
+        </a>
+
+        <!-- Toggler for Mobile View -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+            <!-- Tagline -->
+            <div class="navbar-nav">
+                <span class="nav-link"></span>
+            </div>
+        </div>
+
+        <!-- Login and Signup Buttons -->
+        <div class="d-flex">
+            <button class="btn btn-outline-primary" type="button">Login</button>
+            <button class="btn btn-primary" type="button" style="margin-left: 10px;">Sign Up</button>
+        </div>
+    </div>
+</nav>
