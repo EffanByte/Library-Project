e25ddb44662e941a47ac702784ca545771c67690
arham-729 <aharoon.bscs22seecs@seecs.edu.pk>
2023-12-03 18:38:17 +0500

    Added classes: User, Room, Paper, ...

--- src/classes/user.py
+++ src/classes/user.py
@@ -1,17 +1,34 @@
+import mysql.connector
+
+
 class User:
-    # Constructor, attributes, and methods here...
+    def __init__(self, qalam_id, email, name, password, db_connection):
+        self.qalam_id = qalam_id
+        self.email = email
+        self.name = name
+        self.password = password
+        self.db_connection = db_connection
+
     def get_issued_books(self):
-        # Implement logic to retrieve issued books for a user from the database
-        pass
-    
+        query = "SELECT * FROM IssuedBooks WHERE QalamID = %s"
+        self.db_connection.cursor.execute(query, (self.qalam_id,))
+        issued_books = self.db_connection.cursor.fetchall()
+        return issued_books
+
     def get_reserved_rooms(self):
-        # Logic to retrieve reserved rooms for a user
-        pass
-    
+        query = "SELECT * FROM Room WHERE QalamID = %s AND ReservationStatus = 'Reserved'"
+        self.db_connection.cursor.execute(query, (self.qalam_id,))
+        reserved_rooms = self.db_connection.cursor.fetchall()
+        return reserved_rooms
+
     def report_lost_item(self, item_description, date_reported):
-        # Method to report a lost item
-        pass
-    
+        query = "INSERT INTO LostItems (QalamID, ItemDescription, DateReported) VALUES (%s, %s, %s)"
+        values = (self.qalam_id, item_description, date_reported)
+        self.db_connection.cursor.execute(query, values)
+        self.db_connection.conn.commit()
+
     def report_found_item(self, item_description, date_reported):
-        # Method to report a found item
-        pass
+        query = "INSERT INTO FoundItems (QalamID, ItemDescription, DateReported) VALUES (%s, %s, %s)"
+        values = (self.qalam_id, item_description, date_reported)
+        self.db_connection.cursor.execute(query, values)
+        self.db_connection.conn.commit()
